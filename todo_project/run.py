1bea3bd9b5c52fe447ca3d11a05f857229aeb496
Sara Cheque <saracheque06@gmail.com>
2024-09-22 17:19:37 -0300

    Etapa 7 - v10

--- todo_project/run.py
+++ todo_project/run.py
@@ -1,16 +1,4 @@
 from todo_project import app
-from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
-
-
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
-
-@app.route('/metrics')
-def metrics_endpoint():
-    return metrics.get_metrics()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
